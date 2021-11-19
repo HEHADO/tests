929ad4024aacce9728a32d1730eb4d5cb3eb425b
Anton <79150964023@yandex.ru>
2021-11-20 02:17:29 +0300

    Final wersion

--- My.cpp
+++ My.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 #include <fstream>
-#include <list>
+#include <deque>
 //console colors
 #define COLORENDS      "\x1b[0m"
+#define GREEN          "\x1b[32m"
 #define RED            "\x1b[31m"
 
 using std::cin; 
 using std::cout;
-// крнтейнер с данными
+// контейнер с данными
 class Container {
     public:
-    std::string key, real_key, body;
-    //std::list<std::string> body;
+    std::string key, real_key;
+    std::deque<std::string> body;
+    int count = 0;
 };
-// функция сортировки, аналог qsort для строк
-void quickSort(std::string &str, const int left,const int right) noexcept {
+//новое имя для структуры с контейнерами
+typedef std::deque<Container, std::allocator<Container> > List;
+// функция сортировки, аналог qsort для любых типов данных поддерживающих сравнение
+template <typename T, typename L = int>
+void quickSort(T &str, const int left,const int right) noexcept {
     int i = left, j = right;
-    int mid = str[(left + right) / 2];
+    L mid = str[(left + right) / 2];
     while (i <= j) {
         while (str[i] < mid)
             i++;
@@ -28,19 +33,50 @@ void quickSort(std::string &str, const int left,const int right) noexcept {
         }
     };
     if (left < j)
-        quickSort(str, left, j);
+        quickSort<T, L>(str, left, j);
     if (i < right)
-        quickSort(str, i, right);
+        quickSort<T, L>(str, i, right);
 }
-
-
-void normalize(std::string& s1){
-    //qsort();
-    s1.c_str();
+//функция сортировки для данных
+void normalize(List& list){
+    int i=-1;
+    while (++i < list.size()) {
+        //++i;
+        if (list[i].count==1) continue;
+        //cout<<list[i].key;
+        quickSort<std::deque<std::string>, std::string>(list[i].body,0,list[i].body.size()-1);
+    }
+    return;
+}
+//функция чтения пары ключ-данные
+void readCouple(std::ifstream& file, List& list){
+    Container container;
+    int i = 0;
+    std::string body="";
+    file >> container.key >> body;
+    cout << container.key<< "   "<< body << '\n';
+    if (container.key == "") return;
+    if (body == "") {
+        std::cerr << RED << "bad file\n" << COLORENDS;
+        return;
+    }
+    container.body.push_back(body);
+    container.real_key = container.key;
+    quickSort <std::string>(container.real_key, 0, container.real_key.size()-1);
+    while (i < list.size()) {
+        if(list[i].real_key == container.real_key){
+            list[i].body.push_back(body);
+            ++list[i].count;
+            return;
+        }
+        ++i;
+        }
+    ++list[i].count;
+    list.push_back(container);
 }
 
 int main(int argc, char const *argv[]) {
-    std::list<Container, std::allocator<Container> > list;
+    List list;
     std::ifstream file;
     // открываем файл
     if (argc > 1) {
@@ -57,24 +93,21 @@ int main(int argc, char const *argv[]) {
     }
     // читаем файл  и создаем список контейнеров, в которых хранятся ключи с данными
     while (!file.eof()){
-        Container container;
-        file >> container.key >> container.body;
-        cout << container.key<< "   "<< container.body << '\n';
-        if (container.key == "") break;
-        if (container.body == "") {
-            std::cerr << RED << "bad file\n" << COLORENDS;
-            break;
+        readCouple(file,list);
+    }
+    //сортируем все данные
+    normalize(list);
+    //печатаем результат
+    cout << GREEN << "Result\n" << COLORENDS;
+    for (int i = 0; i < list.size(); ++i){
+        Container container = list[i];
+        cout << container.key << "   ";
+        for (int j = 0; j < container.body.size(); ++j) {
+            cout << container.body[j];
         }
-        container.real_key = container.key;
-        quickSort (container.real_key, 0, container.real_key.size() );
-        list.push_front(container);
+        cout << std::endl;
     }
-    while (list.size()>0) {
-        Container container = list.back();
-        list.pop_back();
-        cout << container.key<< "   "<< container.body << '\n';
-    }
-    return 0;
+    return 0;    
 }
 
 
